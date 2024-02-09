a513a24339f6aa4be7df57a9f84868aa51bce607
demeena <demeena.msk@gmail.com>
2024-02-09 16:12:33 +0300

    установлены права на выполнение для исполняемого файла eslint

--- dist/test.js
+++ dist/test.js
@@ -6,13 +6,3 @@ const form = HexletCode.formFor(template, { method: 'post' }, (f) => {
     f.submit('Wow');
 });
 console.log(form);
-// <div></div>
-/*
- <form action="#" method="post">
-      <label for="name">Name</label>
-      <input name="name" type="text" value="rob">
-      <label for="job">Job</label>
-      <textarea cols="20" rows="40" name="job" as="textarea">hexlet</textarea>
-      <input type="submit" value="Wow">
-  </form>
-*/
